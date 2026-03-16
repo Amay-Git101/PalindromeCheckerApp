355eeeb838b1ee9a5f13453787ba1dcd630e8260
Amay-Git101 <amayjariwala@gmail.com>
2026-03-16 10:35:07 +0530

    UC11: Object-Oriented Palindrome Service

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,40 +1,24 @@
-import java.util.Scanner;
+class PalindromeChecker {
 
-public class PalindromeCheckerApp {
+    public boolean PalindromeCheckerApp(String input) {
 
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a string: ");
-        String input = sc.nextLine();
-
-        // Step 1: Normalize the string
+        // Normalize string
         String normalized = input.replaceAll("\\s+", "").toLowerCase();
 
-        // Step 2: Convert to char array
+        // Convert to char array
         char[] arr = normalized.toCharArray();
 
-        // Step 3: Two pointer comparison
         int start = 0;
         int end = arr.length - 1;
-        boolean isPalindrome = true;
 
-        while(start < end){
-            if(arr[start] != arr[end]){
-                isPalindrome = false;
-                break;
+        while(start < end) {
+            if(arr[start] != arr[end]) {
+                return false;
             }
             start++;
             end--;
         }
 
-        // Step 4: Result
-        if(isPalindrome)
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not a Palindrome");
-
-        sc.close();
+        return true;
     }
 }
